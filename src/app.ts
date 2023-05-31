a5fad0d7b38dcd27b37527e74243054709436d6a
Ben <benjencheng62@gmail.com>
2023-05-31 11:24:40 +0800

    establish socket server at port 3002

--- src/app.ts
+++ src/app.ts
@@ -4,23 +4,71 @@ import cors from "cors";
 import signUpRouter from "./routes/signUpRouter";
 import loginRouter from "./routes/userRouter";
 import { AppDataSource } from "./connection/dataSource";
+import { Server, Socket } from "socket.io";
+import { createServer } from "http";
+
+const userSocketMap = new Map<string, string>();
 
 (async function () {
+  const webSocketApp = express();
+  const httpServer = createServer(webSocketApp);
+  const webSocketServer = new Server(httpServer, {
+    cors: {
+      origin: "http://localhost:3000",
+      methods: ["GET", "POST"],
+    },
+  });
+
+  webSocketServer.on("connect", (socket: Socket) => {
+    console.log("a user connected");
+
+    socket.on("register", ({ userEmail }) => {
+      console.log(`userEmail: ${userEmail} connected`);
+      userSocketMap.set(userEmail, socket.id);
+    });
+
+    socket.on("private_message", ({ toUserEmail, content }) => {
+      const toSocketId = userSocketMap.get(toUserEmail);
+      if (toSocketId) {
+        socket.to(toSocketId).emit(content);
+      } else {
+        console.error(`user email not exist: ${toUserEmail}`);
+      }
+    });
+
+    socket.on("client_message", (obj) => {
+      console.log(JSON.stringify(obj));
+    });
+
+    socket.on("disconnect", () => {
+      console.log("user disconnected");
+      for (const [userId, socketId] of userSocketMap.entries()) {
+        if (socketId === socket.id) {
+          userSocketMap.delete(userId);
+        }
+      }
+    });
+  });
+
+  httpServer.listen(3002, () => {
+    console.log("WebSocket server listening on 3002 port");
+  });
+
   await AppDataSource.initialize();
 
-  const app = express();
+  const apiApp = express();
 
-  app.use(express.json());
-  app.use(
+  apiApp.use(express.json());
+  apiApp.use(
     cors({
       origin: "http://localhost:3000",
     })
   );
 
-  app.use("/signUp", signUpRouter);
-  app.use("/user", loginRouter);
+  apiApp.use("/signUp", signUpRouter);
+  apiApp.use("/user", loginRouter);
 
-  app.listen(3001, () => {
-    console.log("server is running");
+  apiApp.listen(3001, () => {
+    console.log("API server is running on 3001 port");
   });
 })();
